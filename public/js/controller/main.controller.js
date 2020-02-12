76c3b4ca37e12ee0401835eb2c08c274e2369d1f
Jeremias ramirez <jereramirez499@gmail.com>
2020-02-12 06:48:15 -0400

    agregando buton de ver documentacion

--- public/js/controller/main.controller.js
+++ public/js/controller/main.controller.js
@@ -62,6 +62,11 @@ app.controller("main", ["$scope", function($scope){
 
   }
 
+  $scope.documentation = function(){
+
+    floatNotificationInfo("klk")
+
+  }
 
 
   $scope.displayUpdate = function(key){
